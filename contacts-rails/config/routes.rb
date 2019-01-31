9b6eedf256372a7709bbb933b8e63af54fd4947b
Katia <kat_camp@hotmail.com>
2019-01-31 23:50:34 +0000

    create new contacts

--- contacts-rails/config/routes.rb
+++ contacts-rails/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+  
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+  get '/users' => 'users#index'
+  get '/users/edit' => 'users#edit'
+  post '/users/edit' => 'users#update'
+  get '/users/ delete' => 'users#delete'
+  
+  
 end
