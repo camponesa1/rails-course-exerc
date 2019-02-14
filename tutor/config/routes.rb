7ed4fb01dabd2821bc97768195782143c6b81491
Katia <kat_camp@hotmail.com>
2019-02-14 16:54:38 +0000

    simple_calendar addition and meeting

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :meetings
+  devise_for :users
+  
+  
+  root 'users#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
